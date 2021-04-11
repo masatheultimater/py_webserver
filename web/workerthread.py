b7a3af2784fa456610176c3492f5abc237fdaa27
masatheultimater <mysnowsmile@gmail.com>
2021-04-12 02:40:48 +0900

    #2 UPDATE show now date in dynamic HTML

--- web/workerthread.py
+++ web/workerthread.py
@@ -1,9 +1,10 @@
 import os
+import textwrap
 import traceback
 from datetime import datetime
 from socket import socket
 from threading import Thread
-from typing import Tuple
+from typing import Tuple, Optional
 
 
 class WorkerThread(Thread):
@@ -50,22 +51,51 @@ class WorkerThread(Thread):
       # parse the HTTP request
       method, path, http_version, request_header, request_body = self.parse_http_request(request)
 
-      # create response body with static file
-      try:
-        # create response body from static file
-        response_body = self.get_static_file_content(path)
-      
+      # give anotations to the variables of request
+      response_body: bytes
+      content_type: Optional[str]
+      response_line: str
+
+      # create HTML which shows now date if request path is /now
+      if path == "/now":
+        html = f"""\
+          <html>
+          <body>
+            <h1>Now: {datetime.now()}</h1>
+          </body>
+          </html>
+        """
+        response_body = textwrap.dedent(html).encode()
+        
+        # designate Content-Type
+        content_type = "text/html"
+
         # create response line
         response_line = "HTTP/1.1 200 OK\r\n"
+      
+      # create static HTML if the path is not \now
+      else:
+        try:
+          # create response body from static file
+          response_body = self.get_static_file_content(path)
+          
+          # desinate Content-Type
+          content_type = None
+        
+          # create response line
+          response_line = "HTTP/1.1 200 OK\r\n"
 
-      # TODO: distinguish more detailed sub class exception(e.g. FileNotFound, ISADirectory..)
-      except OSError:
-        # case of file not found
-        response_body = b"<html><body><h1>404 Not Found!</h1></body></html>"
-        response_line = "HTTP/1.1 404 Not Found\r\n"
+        # TODO: distinguish more detailed sub class exception(e.g. FileNotFound, ISADirectory..)
+        except OSError:
+          # return 404 error in case of file not found
+          traceback.print_exc()
+
+          response_body = b"<html><body><h1>404 Not Found!</h1></body></html>"
+          content_type = "text/html"
+          response_line = "HTTP/1.1 404 Not Found\r\n"
       
       # create response header
-      response_header = self.build_response_header(path, response_body)
+      response_header = self.build_response_header(path, response_body, content_type)
       
       # create whole response with joining headers and body
       response = (response_line + response_header + "\r\n").encode() + response_body
@@ -78,6 +108,7 @@ class WorkerThread(Thread):
       # output error logs in console, and keep processing
       print("=== Worker: Error occured in processing request ===")
       traceback.print_exc()
+    
     finally:
       # terminate the connection(whenver exception has occured or not)
       print(f"=== Worker: terminate the interaction with client, remote_address: {self.client_address} ===")
@@ -92,6 +123,7 @@ class WorkerThread(Thread):
     4. request_header: bytes
     5. request_body: bytes
     """
+    
     # parse the whole request into line, header, body
     request_line, remain = request.split(b"\r\n", maxsplit=1)
     request_header, request_body = remain.split(b"\r\n\r\n", maxsplit=1)
@@ -106,6 +138,7 @@ class WorkerThread(Thread):
     """
     - get static file contents from request path
     """
+    
     # delete front / and make it relative path
     relative_path = path.lstrip("/")
     # get the file path
@@ -114,20 +147,25 @@ class WorkerThread(Thread):
     with open(static_file_path, "rb") as f:
       return f.read()
   
-  def build_response_header(self, path: str, response_body: bytes) -> str:
+  def build_response_header(self, path: str, response_body: bytes, content_type: Optional[str]) -> str:
     """
     - build response header
     """
-    # get Content-Type from request data
-    ## get extention from path
-    if "." in path:
-      ext = path.rsplit(".", maxsplit=1)[-1]
-    else:
-      ext =""
     
-    ## get MIME-Type from ext
-    ## give it octet-stream if the ext is not corresponded
-    content_type = self.MIME_TYPES.get(ext, "application/octet-stream")
+    # get Content-Type from request data
+    ## if the Content-Type is not designated
+    if content_type is None:
+      # get extension from path
+      if "." in path:
+        ext = path.rsplit(".", maxsplit=1)[-1]
+      else:
+        ext =""
+      # get MIME-Type from ext
+      # give it octet-stream if the ext is not corresponded
+      content_type = self.MIME_TYPES.get(ext, "application/octet-stream")
+      
+    
+
     
     # create response header
     response_header = ""
