b1340de18c0145582c309839fc8f350357bbe5b2
masatheultimater <mysnowsmile@gmail.com>
2021-04-14 00:37:08 +0900

    #4 CHANGE use dataclass in views.py

--- web/views.py
+++ web/views.py
@@ -4,14 +4,10 @@ from datetime import datetime
 from pprint import pformat
 from typing import Optional, Tuple
 
+from util.http.request import HTTPRequest
+from util.http.response import HTTPResponse
 
-def now(
-  method: str,
-  path: str,
-  http_version: str,
-  request_header: dict,
-  request_body: bytes,  
-) -> Tuple[bytes, Optional[str], str]:
+def now(request: HTTPRequest) -> HTTPResponse:
   """
   - create dynamic HTML that shows now date
   """
@@ -24,24 +20,15 @@ def now(
     </html>
   """
 
-  response_body = textwrap.dedent(html).encode()
+  body = textwrap.dedent(html).encode()
   
   # designate Content-Type
   content_type = "text/html; charset=UTF-8"
-
-  # create response line
-  response_line = "HTTP/1.1 200 OK\r\n"
   
-  return response_body, content_type, response_line
+  return HTTPResponse(body=body, content_type=content_type, status_code=200)
 
 
-def show_request(
-  method: str,
-  path: str,
-  http_version: str,
-  request_header: dict,
-  request_body: bytes,
-) -> Tuple[bytes, Optional[str], str]:
+def show_request(request: HTTPRequest) -> HTTPResponse:
   """
   - create dynamic HTML that shows contents of HTTP request
   """
@@ -51,48 +38,39 @@ def show_request(
     <body>
       <h1>Request Line:</h1>
       <p>
-        {method} {path} {http_version}
+        {request.method} {request.path} {request.http_version}
       </p>
       <h1>Headers:</h1>
-      <pre>{pformat(request_header)}</pre>
+      <pre>{pformat(request.header)}</pre>
       <h1>Body:</h1>
-      <pre>{request_body.decode("utf-8", "ignore")}</pre>
+      <pre>{request.body.decode("utf-8", "ignore")}</pre>
 
     <body>
     <html>
   """ 
   
-  response_body = textwrap.dedent(html).encode()
+  body = textwrap.dedent(html).encode()
 
   # designate Content-Type
   content_type = "text/html; charset=UTF-8"
-
-  # create response line
-  response_line = "HTTP/1.1 200 OK\r\n"
   
-  return response_body, content_type, response_line
+  return HTTPResponse(body=body, content_type=content_type, status_code=200)
 
 
-def parameters(
-  method: str,
-  path: str,
-  http_version: str,
-  request_header:dict,
-  request_body: bytes,
-) -> Tuple[bytes, Optional[str], str]:
+def parameters(request: HTTPRequest) -> HTTPResponse:
   """
   - show POST parameters in HTTP request
   """
   
   # return 405 error 
-  if method == "GET":
-    response_body = b"<html><body><h1>405 Method Not Allowed!</h1></body></html>"
+  if request.method == "GET":
+    body = b"<html><body><h1>405 Method Not Allowed!</h1></body></html>"
     content_type = "text/html; charset=UTF-8"
-    response_line = "HTTP/1.1 405 Method Not Allowed\r\n"
+    status_code = 405
 
-  elif method == "POST":
+  elif request.method == "POST":
     # decode the body & parse decoded query parameters(str)
-    post_params = urllib.parse.parse_qs(request_body.decode())
+    post_params = urllib.parse.parse_qs(request.body.decode())
     html = f"""\
       <html>
       <body>
@@ -102,12 +80,11 @@ def parameters(
       <html>
     """
     
-    response_body = textwrap.dedent(html).encode()
+    body = textwrap.dedent(html).encode()
 
     # designate Content-Type
     content_type = "text/html; charset=UTF-8"
 
-    # create response line
-    response_line = "HTTP/1.1 200 OK\r\n"
+    status_code = 200
 
-  return response_body, content_type, response_line
+  return HTTPResponse(body=body, content_type=content_type, status_code=status_code)
