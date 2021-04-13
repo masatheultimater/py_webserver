741855ca7a7ced13fb08c550f5da427fbd25cf29
masatheultimater <mysnowsmile@gmail.com>
2021-04-14 05:45:20 +0900

    #7 REFACT template engine corespond to other root path

--- views.py
+++ views.py
@@ -23,29 +23,11 @@ def show_request(request: HTTPRequest) -> HTTPResponse:
   """
   - create dynamic HTML that shows contents of HTTP request
   """
- 
-  html = f"""\
-    <html>
-    <body>
-      <h1>Request Line:</h1>
-      <p>
-        {request.method} {request.path} {request.http_version}
-      </p>
-      <h1>Headers:</h1>
-      <pre>{pformat(request.header)}</pre>
-      <h1>Body:</h1>
-      <pre>{request.body.decode("utf-8", "ignore")}</pre>
-
-    <body>
-    <html>
-  """ 
   
-  body = textwrap.dedent(html).encode()
-
-  # designate Content-Type
-  content_type = "text/html; charset=UTF-8"
+  context = {"request": request, "headers": pformat(request.headers), "body": request.body.decode("utf-8", "ignore")}
+  body = render("show_request.html", context)
   
-  return HTTPResponse(body=body, content_type=content_type, status_code=200)
+  return HTTPResponse(body=body)
 
 
 def parameters(request: HTTPRequest) -> HTTPResponse:
@@ -56,50 +38,24 @@ def parameters(request: HTTPRequest) -> HTTPResponse:
   # return 405 error 
   if request.method == "GET":
     body = b"<html><body><h1>405 Method Not Allowed!</h1></body></html>"
-    content_type = "text/html; charset=UTF-8"
-    status_code = 405
-
+    
+    return HTTPResponse(body=body, status_code=405)
+    
   elif request.method == "POST":
     # decode the body & parse decoded query parameters(str)
-    post_params = urllib.parse.parse_qs(request.body.decode())
-    html = f"""\
-      <html>
-      <body>
-        <h1>Parameters:</h1>
-        <pre>{pformat(post_params)}</pre>
-      <body>
-      <html>
-    """
     
-    body = textwrap.dedent(html).encode()
-
-    # designate Content-Type
-    content_type = "text/html; charset=UTF-8"
-
-    status_code = 200
-
-  return HTTPResponse(body=body, content_type=content_type, status_code=status_code)
+    context = {"params": urllib.parse.parse_qs(request.body.decode())}
+    body = render("parameters.html", context)
+    
+    return HTTPResponse(body=body)
 
 
 def user_profile(request: HTTPRequest) -> HTTPResponse:
   """
   - return REST style user resource 
   """
-  user_id = request.params["user_id"]
-  html = f"""\
-    <html>
-    <body>
-      <h1>Profile</h1>
-      <p>ID: {user_id}</p>
-    </body>
-    </html>
-  """
-  body = textwrap.dedent(html).encode()
-  content_type = "text/html; charset=UTF-8"
-  status_code = 200
+
+  context = {"user_id": request.params["user_id"]}
+  body = render("user_profile.html", context)
   
-  return HTTPResponse(
-    body=body,
-    content_type=content_type,
-    status_code=status_code
-  )
+  return HTTPResponse(body=body)
