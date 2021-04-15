15acc8d37da706c91772924b39a1dd44c3fb604c
masatheultimater <mysnowsmile@gmail.com>
2021-04-15 17:18:14 +0900

    #8 ADD cookies dict in response dataclass

--- util/http/response.py
+++ util/http/response.py
@@ -6,6 +6,7 @@ from typing import Optional, Union, Dict
 class HTTPResponse:
   status_code: int
   headers: dict
+  cookies: dict
   content_type: Optional[str]
   body: Union[bytes, str]
   
@@ -13,13 +14,17 @@ class HTTPResponse:
     self,
     status_code: int = 200,
     headers: dict = None,
+    cookies: dict = None,
     content_type: str = None,
     body: Union[bytes, str] = b""
   ):
     if headers is None:
       headers = {}
+    if cookies is None:
+      cookies = {}
 
     self.status_code = status_code
     self.headers = headers
+    self.cookies = cookies
     self.content_type = content_type
     self.body = body
